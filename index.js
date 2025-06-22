b86d100e65612555fe7e241d512a12ffea7b9374
Elijah Bustamante <124417882+choybuch@users.noreply.github.com>
2025-06-22 21:48:00 +0800

    abc

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var cors = require('cors');
+var multer  = require('multer');
 require('dotenv').config()
 
 var app = express();
+var upload = multer({ dest: 'uploads/' });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,6 +13,16 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 
 
